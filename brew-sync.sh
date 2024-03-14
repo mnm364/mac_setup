cc6f63639059a9ef90c9f0d6b34cccc4d9aadcdf
Michael  Miller <mnm364@gmail.com>
2024-03-14 07:12:01 -0400

    Brewfile-extras to save space

--- brew-sync.sh
+++ brew-sync.sh
@@ -3,5 +3,10 @@
 # Install all apps in the bundle
 brew bundle -q --file "$MAC_SETUP/Brewfile"
 
+echo $1
+if [[ $1 == "extras" ]]; then
+    brew bundle -q --file "$MAC_SETUP/Brewfile-extras"
+fi
+
 # Upgrade all apps to latest
 # brew cu -ay
